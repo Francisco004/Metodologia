01bbb415af15f7402eee1054aa22c1b75a840cb0
Francisco004 <fran_25_02@hotmail.com>
2021-07-24 23:39:25 -0300

    xd

---
+++ TP4 Final/Machaca.Gaston/Fabrica Militar/Informes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+namespace Fabrica_Militar
+{ 
+    public class Informes<T>
+    {
+        private string mensaje;
+        public void GenerarInformeXML(T dato)
+        {
+            mensaje = typeof(T).Name;
+            XmlSerializer s = new XmlSerializer(typeof(T));
+
+            using (TextWriter tw = new StreamWriter($"{mensaje}.xml"))
+            {
+                s.Serialize(tw, dato);
+            }
+        }
+    }
+
+
+}
